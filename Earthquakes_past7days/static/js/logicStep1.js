2769831f6a668d976d0bf658cc5f4aefef7889bd
Apollo619 <ryan48meredith@yahoo.com>
2021-06-01 08:22:54 -0500

    earthquake work

--- Earthquakes_past7days/static/js/logicStep1.js
+++ Earthquakes_past7days/static/js/logicStep1.js
@@ -1,78 +1,6 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-// Create the map object with a center and zoom level.
-// let map = L.map('mapid').setView([30, 30], 2);
-
-
-// Add GeoJSON data.
-// let sanFranAirport =
-// {"type":"FeatureCollection","features":[{
-//     "type":"Feature",
-//     "properties":{
-//         "id":"3469",
-//         "name":"San Francisco International Airport",
-//         "city":"San Francisco",
-//         "country":"United States",
-//         "faa":"SFO",
-//         "icao":"KSFO",
-//         "alt":"13",
-//         "tz-offset":"-8",
-//         "dst":"A",
-//         "tz":"America/Los_Angeles"},
-//         "geometry":{
-//             "type":"Point",
-//             "coordinates":[-122.375,37.61899948120117]}}
-// ]};
-
-// pointToLayer popup
-// L.geoJson(sanFranAirport, {
-//   pointToLayer: function(feature, latlng) {
-//     console.log(feature);
-//     return L.marker(latlng)
-//       .bindPopup("<h2>" + feature.properties.name + "</h2> <hr> <h3>" + feature.properties.city + ", " + feature.properties.country + "</h3>");
-//     }
-// }).addTo(map);
-
-// onEachFeature popup
-// L.geoJson(sanFranAirport, {
-//   onEachFeature: function(feature,layer) {
-//     console.log(layer);
-//     layer.bindPopup("<h2>Airport code: " + feature.properties.faa + "</h2> <hr> <h3>Airport name: " + feature.properties.name + "</h3>");
-//   }
-// }).addTo(map);
-
-// Coordinates for each point to be used in the line.
-// let line = [
-//     [33.9416, -118.4085],
-//     [30.1975, -97.6664],
-//     [25.7951, -80.2785],
-//     [43.6777, -79.6248],
-//     [40.6413, -73.7781]
-//   ];
-// Create a polyline using the line coordinates and make the line red.
-//  L.polyline(line, {
-//     color: "blue",
-//     weight: "4",
-//     dashArray: '20,20'
-//   }).addTo(map);
-
-// call cities.js with variable
-// let cityData = cities;
-
-// Loop through the cities array and create one marker for each city.
-// cityData.forEach(function(city) {
-//     console.log(city)
-//     L.circleMarker(city.location, {
-//         radius: city.population/200000,
-//         color: 'orange',
-
-//     })
-//     .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population: " + city.population.toLocaleString() + "</h3>")
-//     .addTo(map);
-
-// });
-
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -93,12 +21,12 @@ attribution: 'Map data &copy <a href="https://www.openstreetmap.org/">OpenStreet
 // create a base layer that hold both maps
 let baseMaps = {
   Streets: streets,
-  "Satellite Streets": satelliteStreets
+  Satellite: satelliteStreets
 };
 
 let map = L.map('mapid', {
-  center: [43.7, -79.3],
-  zoom: 11,
+  center: [39.5, -98.5],
+  zoom: 3,
   layers: [streets]
 });
 
@@ -106,7 +34,7 @@ let map = L.map('mapid', {
 L.control.layers(baseMaps).addTo(map);
 
 // Accessing the airport GeoJSON URL
-let torontoHoods = "https://raw.githubusercontent.com/Apollo619/Mapping_Earthquakes/main/torontoNeighborhoods.json"
+let earthquakes = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 
 
 // Create a style for the lines.
@@ -118,15 +46,9 @@ let myStyle = {
 }
 
 // Grabbing our GeoJSON data.
-d3.json(torontoHoods).then(function(data) {
+d3.json(earthquakes).then(function(data) {
   console.log(data);
   // Creating a GeoJSON layer with the retrieved data.
-  L.geoJson(data, {
-    style: myStyle,
-    onEachFeature: function(feature,layer) {
-      console.log(layer);
-      layer.bindPopup("<h2>Neighborhood: " + feature.properties.AREA_NAME + "</h2>")
-    }
-  })
+  L.geoJson(data)
   .addTo(map);
 });
